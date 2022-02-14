56c1fbf973517563d407e67980cf4bbd262adfb1
LizMackle <liz.mackle@outlook.com>
2022-02-14 09:52:10 +0800

    badge bug fixed

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,50 +1,27 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  let badge = "";
-  switch (license) {
-    case "MIT":
-      license = "[![License](https://img.shields.io/badge/License-MIT-brightgreen)](https://opensource.org/licenses/MIT)"
-      return badge;
-
-    case "The Unlicense":
-      license = "[![License](https://img.shields.io/badge/License-Unlicense-blue)](https://unlicense.org/)"
-      return badge;
-
-    case "Mozilla":
-      license = "[![License](https://img.shields.io/badge/License-Mozilla-yellow)](https://opensource.org/licenses/MPL-2.0)"
-      return badge;
-
-    case "Eclipse":
-      license = "[![License](https://img.shields.io/badge/License-Eclipse-red)](https://opensource.org/licenses/EPL-2.0)"
-      return badge;
-
-    case "GPL":
-      license = "[![License](https://img.shields.io/badge/License-GPL-lightgrey)](https://opensource.org/licenses/gpl-license)"
-      return badge;
-  }
-}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-// function renderLicenseLink(license) {
-//   let licenseLink = "";
+// function renderLicenseBadge(license) {
+//   let badge = "";
 //   switch (license) {
 //     case "MIT":
-//       licenseLink = "https://opensource.org/licenses/MIT"
-//       return licenseLink;
+//       license = "[![License](https://img.shields.io/badge/License-MIT-brightgreen)](https://opensource.org/licenses/MIT)"
+//       return badge;
+
 //     case "The Unlicense":
-//       licenseLink = "https://unlicense.org/"
-//       return licenseLink;
+//       license = "[![License](https://img.shields.io/badge/License-Unlicense-blue)](https://unlicense.org/)"
+//       return badge;
+
 //     case "Mozilla":
-//       licenseLink = "https://opensource.org/licenses/MPL-2.0"
-//       return licenseLink;
+//       license = "[![License](https://img.shields.io/badge/License-Mozilla-yellow)](https://opensource.org/licenses/MPL-2.0)"
+//       return badge;
+
 //     case "Eclipse":
-//       licenseLink = "https://opensource.org/licenses/EPL-2.0"
-//       return licenseLink;
+//       license = "[![License](https://img.shields.io/badge/License-Eclipse-red)](https://opensource.org/licenses/EPL-2.0)"
+//       return badge;
+
 //     case "GPL":
-//       licenseLink = "https://opensource.org/licenses/gpl-license"
-//       return licenseLink;
+//       license = "[![License](https://img.shields.io/badge/License-GPL-lightgrey)](https://opensource.org/licenses/gpl-license)"
+//       return badge;
 //   }
 // }
 
@@ -125,7 +102,7 @@ function renderLicenseSection(license) {
 function generateMarkdown(data) {
 return `# 09 - NodeJS: ${data.title}⚙️
 
-${renderLicenseBadge(data.license)}
+![License](https://img.shields.io/badge/License-${data.license}-red)
 
 ## Description
 
